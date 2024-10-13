590584498727ddba1b93cfa6c49f0ac8c5a249d0
Justin <justinrcasteel@gmail.com>
2024-10-13 14:58:36 -0400

    added production jsons

--- scripts/generateImageJson.js
+++ scripts/generateImageJson.js
@@ -1,21 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
+// Check for the --production flag
+const isProduction = process.argv.includes('--production');
+const basePath = isProduction ? '/pandj' : '';
+
 const folders = ['turksandcaicos', 'newyork', 'homepage'];
-const basePath = process.env.NODE_ENV === 'production' ? '/pandj' : '';
 
 folders.forEach(folder => {
     const directoryPath = path.join(process.cwd(), 'public', folder);
+    const jsonPath = path.join(process.cwd(), 'public', `${folder}.json`);
+
+    // Read existing JSON file if it exists
+    let existingImages = [];
+    if (fs.existsSync(jsonPath)) {
+        const existingData = fs.readFileSync(jsonPath, 'utf8');
+        existingImages = JSON.parse(existingData);
+    }
+
     fs.readdir(directoryPath, (err, files) => {
         if (err) {
             console.error(`Failed to read directory: ${directoryPath}`, err);
             return;
         }
-        const images = files.filter(file => /\.(jpe?g|png|gif|bmp)$/i.test(file)).map(file => ({
+
+        const newImages = files.filter(file => /\.(jpe?g|png|gif|bmp)$/i.test(file)).map(file => ({
             src: `${basePath}/${folder}/${file}`,
             text: 'Caption'
         }));
-        const jsonPath = path.join(process.cwd(), 'public', `${folder}.json`);
-        fs.writeFileSync(jsonPath, JSON.stringify(images, null, 2));
+
+        // Merge new images with existing images, avoiding duplicates
+        const mergedImages = [...existingImages, ...newImages.filter(newImage => !existingImages.some(existingImage => existingImage.src === newImage.src))];
+
+        fs.writeFileSync(jsonPath, JSON.stringify(mergedImages, null, 2));
     });
 });
