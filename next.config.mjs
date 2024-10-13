2ca938fcef3171959f5a6b16cf83271af9d5baeb
Justin <justinrcasteel@gmail.com>
2024-10-13 00:37:34 -0400

    setup basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: process.env.NODE_ENV === 'production' ? '/pandj' : '',
+};
 
 export default nextConfig;
