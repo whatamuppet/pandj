20812e4a9df03f3b8d7776dede01fd2693758a88
Justin <justinrcasteel@gmail.com>
2024-10-13 00:06:44 -0400

    changed to Card from Image for pages

--- app/newyork/page.js
+++ app/newyork/page.js
@@ -1,29 +1,68 @@
+'use client';
+
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import styles from './page.module.css';
+import Card from 'react-bootstrap/Card';
 
 export default function NewYork() {
+
+    const cardsData = [
+        {
+            src: "homepage/1.png",
+            text: "Caption"
+        },
+        {
+            src: "homepage/1.png",
+            text: "Caption"
+        },
+        {
+            src: "homepage/1.png",
+            text: "Caption"
+        },
+        {
+            src: "homepage/1.png",
+            text: "Caption"
+        },
+        {
+            src: "homepage/1.png",
+            text: "Caption"
+        },
+        {
+            src: "homepage/1.png",
+            text: "Caption"
+        },
+        {
+            src: "homepage/1.png",
+            text: "Caption"
+        },
+        {
+            src: "homepage/1.png",
+            text: "Caption"
+        },
+        {
+            src: "homepage/1.png",
+            text: "Caption"
+        },
+    ];
+
     return (
         <div className={styles.bg}>
             <Container>
-                <Row>
-                    <Col xs={12} md={4}>
-                        <Image src="homepage/1.png" className='my-4' thumbnail />
-                        <Image src="homepage/1.png" thumbnail />
-                        <Image src="homepage/1.png" className='my-4' thumbnail />
-                    </Col>
-                    <Col xs={12} md={4}>
-                        <Image src="homepage/1.png" className='my-4' thumbnail />
-                        <Image src="homepage/1.png" thumbnail />
-                        <Image src="homepage/1.png" className='my-4' thumbnail />
-                    </Col>
-                    <Col xs={12} md={4}>
-                        <Image src="homepage/1.png" className='my-4' thumbnail />
-                        <Image src="homepage/1.png" thumbnail />
-                        <Image src="homepage/1.png" className='my-4' thumbnail />
-                    </Col>
+                <Row xs={1} md={3} className="g-4 p-3">
+                    {cardsData.map((card, idx) => (
+                        <Col key={idx}>
+                            <Card>
+                                <Card.Img variant="top" src={card.src} />
+                                <Card.Body>
+                                    <Card.Text>
+                                        {card.text}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
                 </Row>
             </Container>
         </div>
