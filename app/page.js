754ca218641e840058eff07f2dc71300259420a1
Justin <justinrcasteel@gmail.com>
2024-10-13 18:31:40 -0400

    add homepage videos

--- app/page.js
+++ app/page.js
@@ -2,22 +2,35 @@
 
 import { basePath } from '@/next.config.mjs';
 import { Container } from 'react-bootstrap';
+import styles from './page.module.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function HomePage() {
-
-    const bgStyle = {
-        backgroundImage: `url(${basePath}/homepage/bg.jpg)`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        minHeight: '100vh'
+    const handleVideoClick = (event) => {
+        event.target.currentTime = 0;
+        event.target.play();
     };
 
     return (
-        <div style={bgStyle}>
-            <Container>
-                <h1 className="text-center text-white">Welcome to the homepage!</h1>
+        <>
+            <div className={styles.bodyBackground}></div>
+            <Container fluid className='my-5 text-center'>
+                <Row className='mb-5'>
+                    <Col>
+                        <video autoPlay muted onClick={handleVideoClick}>
+                            <source src={`${basePath}/homepage/map_usa.webm`} type="video/webm" />
+                        </video>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <video autoPlay muted onClick={handleVideoClick}>
+                            <source src={`${basePath}/homepage/map_world.webm`} type="video/webm" />
+                        </video>
+                    </Col>
+                </Row>
             </Container>
-        </div>
+        </>
     );
 }
