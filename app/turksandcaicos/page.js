2e50cabec2f7f5397648ae8113cd276baa21f7d0
Justin <justinrcasteel@gmail.com>
2024-10-14 01:57:47 -0400

    turks and caicos pictures

--- app/turksandcaicos/page.js
+++ app/turksandcaicos/page.js
@@ -17,36 +17,26 @@ export default function TurksAndCaicos() {
             .catch(error => console.error('Error fetching images:', error));
     }, []);
 
-    const bgStyle = {
-        backgroundImage: `url(${basePath}/turksandcaicos/bg.jpg)`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        minHeight: '100vh'
-    };
-
     return (
-        <div style={bgStyle}>
-            <Container>
-                <Row xs={1} md={3} className="g-4 p-3">
-                    {cardsData.map((card, idx) => (
-                        <Col key={idx}>
-                            <Card>
-                                <Card.Img variant="top" src={card.src} />
-                                {(card.title || card.date) && (
-                                    <Card.Body>
-                                        <Card.Text>
-                                            {card.title && <span>Title: {card.title}</span>}
-                                            {card.title && card.date && <br />}
-                                            {card.date && <span>Date: {card.date}</span>}
-                                        </Card.Text>
-                                    </Card.Body>
-                                )}
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
-        </div>
+        <Container>
+            <Row xs={1} md={3} className="g-4 p-3">
+                {cardsData.map((card, idx) => (
+                    <Col key={idx}>
+                        <Card>
+                            <Card.Img variant="top" src={card.src} />
+                            {(card.title || card.date) && (
+                                <Card.Body>
+                                    <Card.Text>
+                                        {card.title && <span>Title: {card.title}</span>}
+                                        {card.title && card.date && <br />}
+                                        {card.date && <span>Date: {card.date}</span>}
+                                    </Card.Text>
+                                </Card.Body>
+                            )}
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     );
 }
