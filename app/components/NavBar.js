828802398204d8e14c26718569e8d873a164bf27
Justin <justinrcasteel@gmail.com>
2024-10-19 02:38:07 -0400

    remove home link from nav

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
     return (
         <Navbar bg="dark" data-bs-theme="dark" className='border-bottom border-info border-2'>
             <Container>
-                <Navbar.Brand className='text-info' href={`${basePath}/`}>P and J</Navbar.Brand>
+                <Navbar.Brand className='text-info'>P and J</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link className='text-white' href={`${basePath}/`}>Home</Nav.Link>
                     <Nav.Link className='text-white' href={`${basePath}/turksandcaicos`}>Turks and Caicos</Nav.Link>
